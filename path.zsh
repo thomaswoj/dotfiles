98e4b740853f1c4b465d6c5068865c849fbb9114
Ashley Shenton <diagnostix@me.com>
2021-10-21 14:32:32 +0100

    update paths

--- path.zsh
+++ path.zsh
@@ -8,9 +8,13 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 export PATH="$HOME/.node/bin:$PATH"
 
 # Use project specific binaries before global ones
-export PATH="node_modules/.bin:vendor/bin:$PATH"
+#export PATH="node_modules/.bin:vendor/bin:$PATH"
 
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+
+# This loads nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
